16215572e27c5c482ac212b53345f64f328ee6f6
mosesvk <kaumatule93@gmail.com>
2023-08-15 10:07:00 -0600

    adding protect middleware to /users/profile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,12 +11,13 @@ import {
     getUserById,
     updateUser
   } from '../controllers/userController.js'
+  import {protect, admin} from '../middleware/authMiddleware.js'
 
 
 router.route('/').get(getUsers).post(registerUser)
 router.post('/logout', logoutUser)
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser)
 
 export default router
