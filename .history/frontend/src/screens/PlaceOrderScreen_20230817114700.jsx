748016ab68f383cd3a74e99de9eda6860c5059a4
mosesvk <kaumatule93@gmail.com>
2023-08-17 18:31:47 -0600

    PlaceOrder.jsx
    working on changing up the UI

---
+++ .history/frontend/src/screens/PlaceOrderScreen_20230817114700.jsx
@@ -0,0 +1,57 @@
+import React, { Fragment, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import CheckoutSteps from "../components/CheckoutSteps";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { useCreateOrderMutation } from "../slices/ordersApiSlice";
+import { clearCartItems } from "../slices/cartSlice";
+
+const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+
+  const [createOrder, { isLoading, error }] = useCreateOrderMutation();
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate("/shipping");
+    } else if (!cart.paymentMethod) {
+      navigate("/payment");
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+
+  const dispatch = useDispatch();
+  const placeOrderHandler = async () => {
+    try {
+      const res = await createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      }).unwrap();
+      dispatch(clearCartItems());
+      navigate(`/order/${res._id}`);
+    } catch (err) {
+      toast.error(err);
+    }
+  };
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <div className="grid grid-cols-2">
+        <div></div>
+        <div></div>
+      </div>
+    </FormContainer>
+  );
+};
+
+export default PlaceOrderScreen;
---
+++ .history/frontend/src/screens/PlaceOrderScreen_20230817115545.jsx
@@ -0,0 +1,128 @@
+import React, { Fragment, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import CheckoutSteps from "../components/CheckoutSteps";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { useCreateOrderMutation } from "../slices/ordersApiSlice";
+import { clearCartItems } from "../slices/cartSlice";
+
+const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+
+  const [createOrder, { isLoading, error }] = useCreateOrderMutation();
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate("/shipping");
+    } else if (!cart.paymentMethod) {
+      navigate("/payment");
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+
+  const dispatch = useDispatch();
+  const placeOrderHandler = async () => {
+    try {
+      const res = await createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      }).unwrap();
+      dispatch(clearCartItems());
+      navigate(`/order/${res._id}`);
+    } catch (err) {
+      toast.error(err);
+    }
+  };
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 grid">
+          <div>
+            <h2>Shipping</h2>
+            <p>
+              <strong>Address:</strong>
+              {cart.shippingAddress.address}, {cart.shippingAddress.city}{" "}
+              {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
+            </p>
+          </div>
+
+          <div>
+            <h2>Payment Method</h2>
+            <strong>Method: </strong>
+            {cart.paymentMethod}
+          </div>
+
+          <div>
+            <h2>Order Items</h2>
+            {cart.cartItems.length === 0 ? (
+              <Message>Your cart is empty</Message>
+            ) : (
+              <div>
+                {cart.cartItems.map((item, index) => (
+                  <div key={index}>
+                    <div md={1}>
+                      <image src={item.image} alt={item.name} fluid rounded />
+                    </div>
+                    <div>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </div>
+                    <div md={4}>
+                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="card col-span-1 w-96 p-4 shadow-xl">
+          <div>
+            <h2>Order Summary</h2>
+          </div>
+          <div>
+            <div>Items</div>
+            <div>${cart.itemsPrice}</div>
+          </div>
+          <div>
+            <div>Shipping</div>
+            <div>${cart.shippingPrice}</div>
+          </div>
+          <div>
+            <div>Tax</div>
+            <div>${cart.taxPrice}</div>
+          </div>
+          <div>
+            <div>Total</div>
+            <div>${cart.totalPrice}</div>
+          </div>
+          <div>
+            {error && <Message variant="danger">{error.data.message}</Message>}
+          </div>
+          <div>
+            <button
+              type="button"
+              className="btn-block"
+              disabled={cart.cartItems === 0}
+              onClick={placeOrderHandler}
+            >
+              Place Order
+            </button>
+            {isLoading && <Loader />}
+          </div>
+        </div>
+      </div>
+    </FormContainer>
+  );
+};
+
+export default PlaceOrderScreen;
